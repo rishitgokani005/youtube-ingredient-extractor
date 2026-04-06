da30a2403fffb49095a720f43565f9c4db7e894c
rishitgokani005 <rishitgokani005@gmail.com>
2026-04-06 14:22:09 +0530

    Delete youtube-ingredients-extractor/scripts directory

--- youtube-ingredients-extractor/scripts/list_formats.py
+++
@@ -1,9 +0,0 @@
-from yt_dlp import YoutubeDL
-
-url = 'https://www.youtube.com/watch?v=N8XBGuHrzro'
-opts = {'listformats': True, 'quiet': False}
-with YoutubeDL(opts) as ydl:
-    try:
-        ydl.extract_info(url, download=False)
-    except Exception as e:
-        print('ERROR:', e)
